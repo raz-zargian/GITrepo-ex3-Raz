046a2e2009b2d0f5c77e3761ca316f49ea44eec6
“raz-zargian <raz.z491@gmail.com>
2026-06-12 03:03:35 +0300

    Updated Raz.java to 3.0 on master branch

--- Raz.java
+++ Raz.java
@@ -1,7 +1,7 @@
 public class Raz {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            System.out.println("My name is Raz 2.0 ..." + i);
+            System.out.println("My name is Raz 3.0 ..." + i);
         }
     }
 }
